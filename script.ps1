15a5031eca660ca9234f34dc4f3ec7fefcd7fc75
eric27014404 <sacag45204@evnft.com>
2022-11-06 19:45:26 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -2,3 +2,4 @@ Initial content for script.ps1
 Temporary commit content for 2022-10-01T19:45:26
 Temporary commit content for 2022-10-02T19:45:26
 Temporary commit content for 2022-10-18T19:45:26
+Temporary commit content for 2022-11-06T19:45:26
