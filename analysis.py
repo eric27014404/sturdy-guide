46b3e2ffa74bc906e0b1fa446ea3aa6341b41053
eric27014404 <sacag45204@evnft.com>
2022-12-04 19:45:26 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -5,3 +5,4 @@ Temporary commit content for 2022-10-28T19:45:26
 Temporary commit content for 2022-10-29T19:45:26
 Temporary commit content for 2022-11-07T19:45:26
 Temporary commit content for 2022-11-30T19:45:26
+Temporary commit content for 2022-12-04T19:45:26
