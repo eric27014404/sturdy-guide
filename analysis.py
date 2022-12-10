2b73723ba71697e001bf17b112ba685981eab2df
eric27014404 <sacag45204@evnft.com>
2022-12-10 19:45:26 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -6,3 +6,4 @@ Temporary commit content for 2022-10-29T19:45:26
 Temporary commit content for 2022-11-07T19:45:26
 Temporary commit content for 2022-11-30T19:45:26
 Temporary commit content for 2022-12-04T19:45:26
+Temporary commit content for 2022-12-10T19:45:26
