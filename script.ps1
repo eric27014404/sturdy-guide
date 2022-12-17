c0628c1346b2d793a4183e383b0479347abc43fa
eric27014404 <sacag45204@evnft.com>
2022-12-17 19:45:26 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -8,3 +8,4 @@ Temporary commit content for 2022-11-14T19:45:26
 Temporary commit content for 2022-11-26T19:45:26
 Temporary commit content for 2022-12-01T19:45:26
 Temporary commit content for 2022-12-11T19:45:26
+Temporary commit content for 2022-12-17T19:45:26
