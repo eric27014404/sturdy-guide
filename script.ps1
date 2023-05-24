d3a340c2e812a1063bf6fea3d5c9f3b742c0c4b1
eric27014404 <sacag45204@evnft.com>
2023-05-24 19:45:26 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -18,3 +18,4 @@ Temporary commit content for 2023-03-29T19:45:26
 Temporary commit content for 2023-04-11T19:45:26
 Temporary commit content for 2023-04-18T19:45:26
 Temporary commit content for 2023-05-07T19:45:26
+Temporary commit content for 2023-05-24T19:45:26
